912a6d1473dd09e0e63b699bcca55e225797438f
ltieshue <lori.tieshue@gmail.com>
2018-02-02 14:59:02 -0700

    Initial Commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,32 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import sample.Model.Task;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+
+
+public class Controller {
+    @FXML
+    private Button clickMeButton;
+
+    @FXML
+    private Text taskHours;
+
+    @FXML
+    private Text employeeListView;
+
+}
+
+
+
+
+
+
+
---
+++ src/sample/Model/Task.java
@@ -0,0 +1,17 @@
+package sample.Model;
+
+public class Task {
+    public String name;
+    public String hoursPerWeek;
+    public int hours;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public Task(String name){
+        this.name = name;
+
+    }
+}
